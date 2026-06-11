38f8b685992105f5f371540e6d46e1f4c8bd99c2
chaanheeLEE <chanhee1519@gmail.com>
2026-06-11 23:15:54 +0900

    feat: 중복되는 상품으로 주문 가능

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.api.service.order;
 
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.controller.order.response.OrderResponse;
@@ -11,6 +12,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +25,22 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         //Product
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        List<Product> products = findProduct(productNumbers);
 
         //Order
         Order order = Order.create(products, registeredDateTime);
         Order saved = orderRepository.save(order);
 
         return OrderResponse.of(saved);
+    }
+
+    private @NonNull List<Product> findProduct(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, product -> product));
+
+        return productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
     }
 }
